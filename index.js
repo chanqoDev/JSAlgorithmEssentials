341dd2b1f63069993aa0ca892eb01b573b0f2d33
Christian E <christianelena92@gmail.com>
2021-09-10 05:58:21 -0500

    Inital Setup for JS Challenges

---
+++ index.js
@@ -0,0 +1,60 @@
+// Example: (!4) Factorial,     denoted as 5!, is 5 x 4 x 3 x 2 x 1 which equals 120. 
+
+// input: number
+// output : number
+// Strategy :  
+
+// CHALLENGE: Factorial of a number
+
+// declare
+function factorial(n) {
+   
+}
+
+
+
+
+// CHALLENGE: Product of a number
+
+
+
+// CHALLENGE: Product of an array
+
+
+// CHALLENGE: Reverse a string
+
+// CHALLENGE: Reverse a number
+
+// CHALLENGE: Sum in Range
+
+
+// CHALLENGE: Sum of numbers in Arr
+
+
+// CHALLENGE: Count letter in string occurance 
+
+// CHALLENGE: Count words
+
+// CHALLENGE: Countdown recursion
+
+// CHALLENGE: Calculate Average / Mean
+
+// CHALLENGE: Diffrence two arrays
+
+// CHALLENGE: Double even in array
+
+// CHALLENGE: Extend objects add property
+
+// CHALLENGE: Search and Destory
+
+// CHALLENGE: Remove Duplicates
+
+// CHALLENGE: Mode of an Array  =>  find the mode: the number that is found the most,    if the number has a tie return the bigger number. 
+
+
+
+
+
+// CHALLENGE: Nested Arr
+
+// CHALLENGE: Neste
