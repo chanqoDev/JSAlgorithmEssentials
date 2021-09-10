88887334afe26ab7cfe3c7b31304a5dd28774f6f
Christian E <christianelena92@gmail.com>
2021-09-10 09:26:06 -0500

    Complete Product of Array and Sum of Array

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Example: (!4) Factorial,     denoted as 5!, is 5 x 4 x 3 x 2 x 1 which equals 120. 
 
 // CHALLENGE: Factorial of a number
+
 // solution A : RECURSIVE 
 // function factorial(n) {
 //  if (!n) return 1; 
@@ -8,10 +9,10 @@
 // }
 
 // solution B : RECURSIVE Tail-Optimization
-// function factorial(n, productTotal = 1) {
-//     if (!n) return productTotal; 
-//     return factorial(Math.abs(n) - 1, productTotal *= n ); 
-// }
+function factorial(n, productTotal = 1) {
+    if (!n) return productTotal; 
+    return factorial(Math.abs(n) - 1, productTotal *= n ); 
+}
 
 // Solution for-loop
 // function factorial(num) {
@@ -24,15 +25,15 @@
 // }
 
 // solution while-loop
-const factorial = (n) => {
-    let product = 1; 
-    if (!n) return product; 
-    while (n > 0) {
-        product *= n; 
-        n = n - 1; 
-    }
-    return product; 
-}
+// const factorial = (n) => {
+//     let product = 1; 
+//     if (!n) return product; 
+//     while (n > 0) {
+//         product *= n; 
+//         n = n - 1; 
+//     }
+//     return product; 
+// }
 
 // Test cases: 
 // console.log(factorial());   // 1
@@ -42,47 +43,55 @@ const factorial = (n) => {
 // console.log(factorial(-7)); // -5040
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ 
+// Write a recursive function called productOfArray that takes in an array of numbers and returns the product of all of the elements.
 
 // CHALLENGE: Product of an array
 
+// Solution : RECURSIVE Tail-Optimization
+// function productOfArr(arr, output = 1) {
+//  if(!arr.length) return output; 
+//  return productOfArr(arr.splice(1), output *= arr[0]); 
+// };
+
+// Solution 2: RECURSION
+// function productOfArr(arr, i = 1) {
+//     if (!arr) return 1; 
+//     return arr[i] * productOfArr(arr[i], i + 1); 
+// }
+
+// Solution : Reduce High-Order Function  
+// const productOfArr = (arr) => arr.reduce((acc, currNum) => acc *= currNum, 1); 
+
+// Solution : For-of Loop 
+// function productOfArr(arr) {
+//     let product = 1; 
+//     for (let num of arr ){ 
+//         product *= num; 
+//     }
+//     return product; 
+// }
+
+// console.log(productOfArr([])); // 1
+// console.log(productOfArr([2, 3, 4])); // 24
+// console.log(productOfArr([2, -3, 5, 6, 9])); // -1620
+ 
 
 
+// BONUS CHALLENGE TIME: Try Yourself doing a sum of array! 
 
+// Write a recursive function sum that calculates the sum of an array of integers.
 
+// function sum(array, acc = 0) {
+// 	if (!array.length) return acc; 
+//   return sum(array.splice(1), acc += array[0]); 
+// }
+// HOF Solution : Sum of array
+// const sum = (arr) => arr.reduce((acc, curr) => acc + curr, 0); 
 
-
-
-
-
-
-
+//  console.log(sum([])) // 0
+//  console.log(sum([1,4,6])); // -> returns 11
+//  console.log(sum([1,2,3,4,5,6])); // -> returns 21
 
 
 
@@ -93,6 +102,30 @@ const factorial = (n) => {
 
 
 // CHALLENGE: Reverse a string
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // CHALLENGE: Reverse a number
 
