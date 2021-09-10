16fbd2580a0a93861efbb3db7b16967dc5d0a340
Christian E <christianelena92@gmail.com>
2021-09-10 08:08:14 -0500

    Complete Factorial challenge 1

--- index.js
+++ index.js
@@ -1,24 +1,95 @@
 // Example: (!4) Factorial,     denoted as 5!, is 5 x 4 x 3 x 2 x 1 which equals 120. 
 
-// input: number
-// output : number
-// Strategy :  
-
 // CHALLENGE: Factorial of a number
+// solution A : RECURSIVE 
+// function factorial(n) {
+//  if (!n) return 1; 
+//  return n * factorial(Math.abs(n) - 1)
+// }
 
-// declare
-function factorial(n) {
-   
+// solution B : RECURSIVE Tail-Optimization
+// function factorial(n, productTotal = 1) {
+//     if (!n) return productTotal; 
+//     return factorial(Math.abs(n) - 1, productTotal *= n ); 
+// }
+
+// Solution for-loop
+// function factorial(num) {
+//    let product = 1; 
+//     if (!num) return product; 
+//     for(let i = num; i > 0; i--) {
+//     product *= i; 
+//  }
+//  return product; 
+// }
+
+// solution while-loop
+const factorial = (n) => {
+    let product = 1; 
+    if (!n) return product; 
+    while (n > 0) {
+        product *= n; 
+        n = n - 1; 
+    }
+    return product; 
 }
 
+// Test cases: 
+// console.log(factorial());   // 1
+// console.log(factorial(0));  // 1
+// console.log(factorial(2));  // 2
+// console.log(factorial(5));  // 120
+// console.log(factorial(-7)); // -5040
 
 
 
-// CHALLENGE: Product of a number
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 // CHALLENGE: Product of an array
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // CHALLENGE: Reverse a string
